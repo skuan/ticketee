00d21c17b5865a185130e707172cc06663f2009b
Benny C. Wong <bwong337@gmail.com>
2014-09-24 15:40:14 -0700

    'Create a new project' feature complete.

---
+++ db/migrate/20140924222915_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration
+  def up
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+  def down
+    drop_table :projects
+  end
+end
