49eeeb3f9cf0f544a70dd72b19ccf751c37f83ce
Raymond Gan <rayning@yahoo.com>
2014-10-01 23:41:10 -0700

    Finished Chp 5 (Nested Resources)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,58 +1,54 @@
 class ProjectsController < ApplicationController
-	before_action :set_project, only: [:show,
+  before_action :set_project, only: [:show,
                                    :edit,
                                    :update,
                                    :destroy]
-	def index
-	  @projects = Project.all
-	end
-	
-	def edit
-	  @project = Project.find(params[:id])
-	end
-	
-	def show
-	  @project = Project.find(params[:id])
-	end
+  def index
+    @projects = Project.all
+  end
 
-	def update
-	  @project = Project.find(params[:id])
-	  if @project.update(project_params)
-	    flash[:notice] = "Project has been updated."
-	    redirect_to @project
-	  else
-	    flash[:alert] = "Project has not been updated."
-	    render "edit"
-	  end
-	end
+  def edit
+  end
 
-	def new
-	  @project = Project.new
-	end
+  def show
+  end
 
-	def create
-	  @project = Project.new(project_params)
-	  if @project.save
-	    flash[:notice] = "Project has been created."
-	    redirect_to @project
-	  else
-	    flash[:alert] = "Project has not been created."
-	    render "new"
-	  end
-	end
+  def update
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render "edit"
+    end
+  end
 
-	def destroy
-	  @project = Project.find(params[:id])
-	  @project.destroy
-	  flash[:notice] = "Project has been destroyed."
-	  redirect_to projects_path
-	end
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been created."
+      render "new"
+    end
+  end
+
+  def destroy
+    @project.destroy
+    flash[:notice] = "Project has been destroyed."
+    redirect_to projects_path
+  end
 
 private
 
-	def project_params
-		params.require(:project).permit(:name, :description)
-	end
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 private
   def set_project
     @project = Project.find(params[:id])
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -5,7 +5,8 @@ feature "Creating Tickets" do
     visit '/'
     click_link "Internet Explorer"
     click_link "New Ticket"
-end
+  end
+
   scenario "Creating a ticket" do
     fill_in "Title", with: "Non-standards compliance"
     fill_in "Description", with: "My pages are ugly!"
@@ -17,7 +18,7 @@ end
     expect(page).to have_content("Ticket has not been created.")
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Description can't be blank")
-  end 
+  end
 
   scenario "Description must be longer than 10 characters" do
     fill_in "Title", with: "Non-standards compliance"
