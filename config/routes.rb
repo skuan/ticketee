3c420297b9d6d774fc1d6c6de35e1528b23f0b91
Raymond Gan <rayning@yahoo.com>
2014-10-08 22:19:02 -0700

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tickets
   end
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
